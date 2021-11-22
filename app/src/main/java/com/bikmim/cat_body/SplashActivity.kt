a4a93b44b9eb42690cd8965a9b8e885ac60a20c6
BIKMIM <2cp4us@gmail.com>
2021-11-22 22:03:19 +0900

    첫 스플래쉬 하면에 경고문구 만들었음. 상태 및 진단 내용 수정 및 추가

--- app/src/main/java/com/bikmim/cat_body/SplashActivity.kt
+++ app/src/main/java/com/bikmim/cat_body/SplashActivity.kt
@@ -1,8 +1,12 @@
 package com.bikmim.cat_body
 
+import android.content.DialogInterface
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.LayoutInflater
+import android.widget.Button
+import androidx.appcompat.app.AlertDialog
 import com.bikmim.cat_body.databinding.ActivitySplashBinding
 
 class SplashActivity : AppCompatActivity() {
@@ -15,11 +19,26 @@ class SplashActivity : AppCompatActivity() {
         sBinding = ActivitySplashBinding.inflate(layoutInflater)
         setContentView(sBinding.root)
 
-        // Splash -> Main Activity로 이동
-
+        // 시작 버튼 눌렸을 때 popup 실행
         sBinding.splashStartBtn.setOnClickListener {
+            // Dialog 만들기
+            val mDialogView = LayoutInflater.from(this).inflate(R.layout.readme, null)
+            val mBuilder = AlertDialog.Builder(this)
+                .setView(mDialogView)
+                .setTitle("주의 사항")
             val intent = Intent(this, MainActivity::class.java)
-            startActivity(intent)
+
+            mBuilder.apply {
+                setPositiveButton("예",
+                    DialogInterface.OnClickListener { dialog, id ->
+                        startActivity(intent)
+                    })
+                setNegativeButton("아니오",
+                    DialogInterface.OnClickListener { dialog, id ->
+                        // User cancelled the dialog
+                    })
+                mBuilder.show()
+            }
         }
     }
 }
